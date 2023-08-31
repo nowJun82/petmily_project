76e379b375edb61d0d901670b2e4479e4dc65bf9
seongghun <esd2727@gmail.com>
2023-08-31 11:44:19 +0900

    질문 ì상세

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,4 +13,13 @@ public class QuestionService {
     public List<Question> getList(){
         return this.questionRepository.findAll();
     }
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
 }
