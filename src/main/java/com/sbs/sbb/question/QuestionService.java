1b80978bc44dae5ee509841706b12ef4b2cdbf6e
seongghun <esd2727@gmail.com>
2023-09-01 11:07:43 +0900

    질ë문 등록과 폼

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -3,6 +3,7 @@ package com.sbs.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,9 +11,11 @@ import java.util.Optional;
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
-    public List<Question> getList(){
+
+    public List<Question> getList() {
         return this.questionRepository.findAll();
     }
+
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
         if (question.isPresent()) {
@@ -22,4 +25,13 @@ public class QuestionService {
         }
     }
 
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
+    }
 }
+
+
