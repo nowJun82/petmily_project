8088d7d101594038bd9f5ff42fd34146cc348e6b
seongghun <esd2727@gmail.com>
2023-09-04 09:16:37 +0900

    공통 템플릿

--- src/main/java/com/sbs/sbb/answer/AnswerRepository.java
+++ src/main/java/com/sbs/sbb/answer/AnswerRepository.java
@@ -1,6 +1,5 @@
 package com.sbs.sbb.answer;
 
-import com.sbs.sbb.answer.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
