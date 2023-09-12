ec83ebade422f3194fc3427950f00ff24c2f81a9
seongghun <esd2727@gmail.com>
2023-09-12 17:35:05 +0900

    pull전 commit

--- src/main/java/com/petmily/user/UserController.java
+++ src/main/java/com/petmily/user/UserController.java
@@ -21,12 +21,12 @@ public class UserController {
     @GetMapping("/inquiry")
     public String inquiry(){return "user_inquiry";}
 
-    @GetMapping("/signup")
-    public String signup(UserCreateForm userCreateForm) {
+    @GetMapping("/login")
+    public String login(UserCreateForm userCreateForm){
         return "login_form";
     }
 
-    @PostMapping("/signup")
+    @PostMapping("/login")
     public String signup(@Valid UserCreateForm userCreateForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "login_form";
@@ -37,10 +37,18 @@ public class UserController {
                     "2개의 패스워드가 일치하지 않습니다.");
             return "login_form";
         }
-
-        userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(),
-                userCreateForm.getEmail(), userCreateForm.getNickname());
-
+        try {
+            userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(),
+                    userCreateForm.getEmail(), userCreateForm.getNickname());
+        }catch(DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "login_form";
+        }catch(Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "login_form";
+        }
         return "redirect:/";
     }
 }
